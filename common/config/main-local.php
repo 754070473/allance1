5ad924d9960ceb2732e600b9676b06be7ffee69e
huxuebao <hxb1475425379@163.com>
2016-06-14 10:45:37 +0800

    添加show.html

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,10 +3,11 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=allance',
             'username' => 'root',
-            'password' => '',
+            'password' => '147542',
             'charset' => 'utf8',
+            //'tablePrefix' => 'al_',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
