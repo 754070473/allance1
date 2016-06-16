94578b495ab8bd3ef30bb4e2767aab9d7dfd7196
xing <1448868980@qq.com>
2016-06-16 18:42:57 +0800

    修改密码

--- frontend/controllers/PublicController.php
+++ frontend/controllers/PublicController.php
@@ -23,6 +23,7 @@ class PublicController extends Controller
 	
 	//public $layout='public';
     public $layout=false;
+    public $enableCsrfValidation=false;
 	/**
      * [actionLeft 主界面]
      * @return [type] [description]
@@ -37,8 +38,10 @@ class PublicController extends Controller
      */
     public function actionTop()
     {
+
         return $this->render('top.html');
     }
+   
     /**
      * [actionLeft 左侧菜单栏]
      * @return [type] [description]
