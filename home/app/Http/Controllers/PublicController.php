0f1069307aeec75980190e24350c93ae636398d4
huxuebao <hxb1475425379@163.com>
2016-06-21 16:02:10 +0800

    提交新的模板 Signed-off-by: huxuebao <hxb1475425379@163.com>

--- home/app/Http/Controllers/PublicController.php
+++ home/app/Http/Controllers/PublicController.php
@@ -8,16 +8,16 @@ namespace App\Http\Controllers;
 class PublicController extends Controller {
 	
 	//展示头部
-	public  function top(){
-		return view("public.top");
-	}
-	/**
-	 * [left 左侧]
-	 * @return [type] [description]
-	 */
-	public  function left(){
-		return view("public.left");
-	}
+	// public  function top(){
+	// 	return view("public.top");
+	// }
+	// /**
+	//  * [left 左侧]
+	//  * @return [type] [description]
+	//  */
+	// public  function left(){
+	// 	return view("public.left");
+	// }
 	/**中心*/
 	public  function main(){
 		return view("public.main");
