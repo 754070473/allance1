fed9edb932f9e50c8ee84c690cbcefdf80fb6ccc
llb <18612815850@163.com>
2016-06-22 21:50:28 +0800

    digital

--- home/app/Http/Controllers/PostofficeController.php
+++ home/app/Http/Controllers/PostofficeController.php
@@ -1,6 +1,11 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use Request,Validator,DB;
+use App\Http\Controllers\Controller;
+use App\Models\Alpost;
+
 /**
  *   PostofficeController  发布职位
  */
@@ -9,9 +14,17 @@ class PostofficeController extends Controller{
 
 	//发布新职位
 	public function create()
-	{
-		return view("postoffice.create");
+	{   
+        $Alpost = new Alpost;
+
+        $res = Alpost::all();
+        //递归处理数据
+        $arr=$Alpost->digui($res);
+        print_r($arr);die;
+		return view("postoffice.create",['arr'=>$arr]);
 	}
+
+	
 
 	//职位发布成功
 	public function index06()
