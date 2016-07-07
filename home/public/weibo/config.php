72ff4da407b214c2bbb378f3b1ce89f307be8757
huxuebao <hxb1475425379@163.com>
2016-07-07 08:17:39 +0800

    sadasdsadsa Signed-off-by: huxuebao <hxb1475425379@163.com>

---
+++ home/public/weibo/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '2465300444' );
+define( "WB_SKEY" , 'f00255f4f7b7d1db99a1c9602e60c9f5' );
+define( "WB_CALLBACK_URL" , 'http://xxxxxxxxxxxx/callback.php' );
